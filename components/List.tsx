d6c9da76dbbd0cdb169a24c8a00d2940fe68c6ab
Mekan89 <40357987+Mekan89@users.noreply.github.com>
2022-06-06 12:53:26 +0500

    Added NavBar Component

--- components/List.tsx
+++
@@ -1,9 +0,0 @@
-const List = ({ title }) => {
-  return (
-    <li className=" underline underline-offset-8 decoration-2 decoration-bgCol hover:text-titleCol hover:decoration-linkCol ">
-      <a href="https://github.com/explore">{title}</a>
-    </li>
-  );
-};
-
-export default List;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,17 @@
 module.exports = {
   content: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    container: { center: true },
     extend: {
       colors: {
         bgCol: "#22272d",
         navCol: "#2d333b",
-        linkCol: "#316dca",
-        titleCol: "hsl(209,19%,73%,1)",
+        linkCol: "#dd7f61",
+        titleCol: "#d3d4d6",
         pryCol: "#758390",
         borderCol: "#454c56",
       },
     },
   },
-  plugins: [require("daisyui")],
+  plugins: [],
 };
